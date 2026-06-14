31d62b5f6b1493ee568e166ca98306ff3c9d9547
drumsaudioguy <inderpals@furtados.in>
2026-06-14 14:03:19 +0530

    Update scheduler.py

--- scheduler.py
+++ scheduler.py
@@ -1,7 +1,32 @@
 import os
+import pandas as pd
+import requests
 
-token = os.getenv("PAGE_ACCESS_TOKEN")
+PAGE_ACCESS_TOKEN = os.getenv("PAGE_ACCESS_TOKEN")
+IG_ACCOUNT_ID = os.getenv("IG_ACCOUNT_ID")
 
-print("TOKEN LENGTH:", len(token))
-print("FIRST 10 CHARS:", token[:10])
-print("LAST 10 CHARS:", token[-10:])
+print("STARTING INSTAGRAM TEST")
+
+df = pd.read_csv("posts.csv")
+
+row = df.iloc[0]
+
+image_url = row["ImageURLs"]
+caption = row["Caption"]
+
+print("Image URL:", image_url)
+print("Caption:", caption)
+
+response = requests.post(
+    f"https://graph.facebook.com/v23.0/{IG_ACCOUNT_ID}/media",
+    data={
+        "image_url": image_url,
+        "caption": caption,
+        "access_token": PAGE_ACCESS_TOKEN
+    }
+)
+
+print("MEDIA RESPONSE")
+print(response.text)
+
+print("FINISHED")
